ee5f129f461a4519437a1ae512141a7fafd14695
Wuhib Bouryou <84643297+wuhibbouryou@users.noreply.github.com>
2021-06-01 15:21:46 -0400

    Update App.java

--- App.java
+++ App.java
@@ -1,17 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is the main app that runs the program
+ * @author Wuhib Bouryou
  */
 package Convert;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-/**
- *
- * @author wuhib
- */
+
 public class App {
         public static void main(String args[]){
             Frame frame = new Frame();
