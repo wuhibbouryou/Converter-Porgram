f5fc60092285dc81e68f99a1fe878fb6362d18ee
Wuhib Bouryou <84643297+wuhibbouryou@users.noreply.github.com>
2021-06-01 15:19:59 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Convert;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author wuhib
+ */
+public class App {
+        public static void main(String args[]){
+            Frame frame = new Frame();
+            frame.show();
+        }
+}
